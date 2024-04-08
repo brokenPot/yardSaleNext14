039193f2b934c6de76df59e34860789f97f4727e
seo jeong woo <seojeongwoo0902@r2ware.com>
2024-04-08 18:00:28 +0900

    5.3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,23 +2,27 @@
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
-
+import {redirect} from "next/navigation";
+import {handleForm} from "@/app/login/actions";
+import { useFormState } from "react-dom";
 export default function LogIn() {
-    async function handleForm(formData: FormData) {
-        "use server";
-        // console.log(formData.get("email"), formData.get("password"));
-        // console.log("i run in the server baby!");
-
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        console.log("logged in!");
-    }
+    // async function handleForm(formData: FormData) {
+    //     "use server";
+    //     // console.log(formData.get("email"), formData.get("password"));
+    //     // console.log("I run in the server baby!");
+    //
+    //     await new Promise((resolve) => setTimeout(resolve, 2000));
+    //     redirect('/')
+    //     console.log("logged in!");
+    // }
+    const [state, action] = useFormState(handleForm, null);
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form action={handleForm} className="flex flex-col gap-3">
+            <form action={action} className="flex flex-col gap-3">
                 <FormInput
                     name="email"
                     type="email"
