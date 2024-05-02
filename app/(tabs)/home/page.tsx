5b1e6f7804c6b7944ea3c011600f0f70a9558104
seo jeong woo <seojeongwoo0902@r2ware.com>
2024-05-02 16:52:46 +0900

    have problem in 13.2

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -5,8 +5,7 @@ import { Prisma } from "@prisma/client";
 import {unstable_cache as nextCache,  revalidatePath } from "next/cache";
 import Link from "next/link";
 
-
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"]); // 60초가 지난후 새로운 요청이 있다면 재 호출
 async function getInitialProducts() {
     const products = await db.product.findMany({
         select: {
@@ -38,12 +37,15 @@ export default async function Products() {
     };
     return (
         <div>
-            <ProductList initialProducts={initialProducts} />
+            <ProductList initialProducts={initialProducts}/>
+            <form action={revalidate}>
+                <button>Revalidate</button>
+            </form>
             <Link
                 href="/products/add"
                 className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
             >
-                <PlusIcon className="size-10" />
+                <PlusIcon className="size-10"/>
             </Link>
         </div>
     );
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -14,19 +14,8 @@ export default function ProductList({ initialProducts }: ProductListProps) {
     const [isLoading, setIsLoading] = useState(false);
     const [page, setPage] = useState(0);
     const [isLastPage, setIsLastPage] = useState(false);
-
-    // const onLoadMoreClick = async () => {
-    //     setIsLoading(true);
-    //     const newProducts = await getMoreProducts(page + 1);
-    //     if (newProducts.length !== 0) {
-    //         setPage((prev) => prev + 1);
-    //         setProducts((prev) => [...prev, ...newProducts]);
-    //     } else {
-    //         setIsLastPage(true);
-    //     }
-    //     setIsLoading(false);
-    // };
     const trigger = useRef<HTMLSpanElement>(null);
+
     useEffect(() => {
         const observer = new IntersectionObserver(
             async (
@@ -60,20 +49,34 @@ export default function ProductList({ initialProducts }: ProductListProps) {
     }, [page]);
 
     return (
-        <div className="p-5 flex flex-col gap-5">
-            {products.map((product:any) => (
-                <ListProduct key={product.id} {...product} />
-            ))}
-            {/*{!isLastPage ? () : null}* */}
-                <span
-                    ref={trigger}
-                    style={{
-                        marginTop: `${page + 1 * 900}vh`,
-                    }}
-                    className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-                >
-                    {isLoading ? "로딩 중" : "Load more"}
+        // <div className="p-5 flex flex-col gap-5">
+        //     {products.map((product: any) => (
+        //         <ListProduct key={product.id} {...product} />
+        //     ))}
+        //     <span
+        //         ref={trigger}
+        //         style={{
+        //             marginTop: `${page + 1 * 900}vh`,
+        //         }}
+        //         className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        //     >
+        //             {isLoading ? "로딩 중" : "Load more"}
+        // </span>
+        // </div>
+
+
+    <div className="p-5 flex flex-col gap-5">
+        {products.map((product) => (
+            <ListProduct key={product.id} {...product} />
+        ))}
+        {/* {!isLastPage ? (
+        <span
+          ref={trigger}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "Load more"}
         </span>
-        </div>
+      ) : null} */}
+    </div>
     );
 }
