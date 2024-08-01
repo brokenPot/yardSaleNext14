a505a05310b366f79d13c36794a3292feeb16862
seo jeong woo <seojeongwoo0902@r2ware.com>
2024-08-01 16:53:30 +0900

    #before self update

---
+++ app/extras/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import view from "../../public/heavy-image.jpeg"
+
+// 폴더 명 앞에 _을 그으면 라우팅을 안할수 있다. -> 비공개 처리 할 수 있다
+export default function Extras() {
+    return (
+        <div className="flex flex-col gap-3 py-10">
+            <h1 className="text-6xl font-metallica">Extras!</h1>
+            <h2 className="font-roboto">So much more to learn!</h2>
+            <Image src={view} alt='' placeholder="blur"/>
+        </div>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: "var(--roboto-text)",
+        rubick: "var(--rubick-text)",
+        metallica: "var(--metallica-text)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
